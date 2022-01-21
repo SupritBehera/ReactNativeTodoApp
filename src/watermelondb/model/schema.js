33b2d99dfebea731ca067d47d8f3f5940f1b4338
Suprit Behera <supritb12@gmail.com>
2022-01-21 19:09:45 +0530

    defined tasks schema

--- src/watermelondb/model/schema.js
+++ src/watermelondb/model/schema.js
@@ -3,6 +3,14 @@ import {appSchema, tableSchema} from '@nozbe/watermelondb';
 export default appSchema({
   version: 1,
   tables: [
-    // We'll add tableSchemas here later
+    tableSchema({
+      name: 'tasks',
+      columns: [
+        {name: 'task_name', type: 'string'},
+        {name: 'is_complete', type: 'boolean'},
+        {name: 'time_created', type: 'string'},
+        {name: 'date_created', type: 'string'},
+      ],
+    }),
   ],
 });
